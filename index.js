dcc2c7aa521c6e8022aade1b6da39cf741c22106
Dharmendra K <dharmendra.hbti@gmail.com>
2022-08-19 02:17:02 +0530

    updated port

--- index.js
+++ index.js
@@ -45,6 +45,6 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(process.env.PORT || PORT, () => {
+server.listen(process.env.PORT, () => {
     console.log(`listening on localhost:${PORT}`);
   });
